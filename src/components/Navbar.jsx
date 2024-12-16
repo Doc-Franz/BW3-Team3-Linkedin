3ed763ccc420cbf93f93d53b6a701b681e6614ee
Doc-Franz <federico.franzi94@gmail.com>
2024-12-16 12:17:03 +0100

    second commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import { Button, Col, Container, Form, InputGroup, Nav, Navbar, NavDropdown, Row } from "react-bootstrap";
+import { Button, Col, Container, Form, Image, InputGroup, Nav, Navbar, NavDropdown, Row } from "react-bootstrap";
 import { Linkedin, Search, HouseDoorFill, PeopleFill, SuitcaseLgFill, ChatDotsFill, BellFill } from "react-bootstrap-icons";
 import { Link } from "react-router-dom";
 
@@ -28,46 +28,63 @@ function MyNavbar() {
                 </Col>
               </Row>
             </Link>
-            <Link to="/home" style={{ textDecoration: "none" }}>
+            <Link to="/rete" style={{ textDecoration: "none" }}>
               <Row className="d-flex flex-column ">
                 <Col className="d-flex justify-content-center">
                   <PeopleFill />
                 </Col>
                 <Col>
-                  <Nav.Link href="#action1">Rete</Nav.Link>
+                  <Nav.Link href="#action2">Rete</Nav.Link>
                 </Col>
               </Row>
             </Link>
-            <Link to="/home" style={{ textDecoration: "none" }}>
+            <Link to="/lavoro" style={{ textDecoration: "none" }}>
               <Row className="d-flex flex-column ">
                 <Col className="d-flex justify-content-center">
                   <SuitcaseLgFill />
                 </Col>
                 <Col>
-                  <Nav.Link href="#action1">Lavoro</Nav.Link>
+                  <Nav.Link href="#action3">Lavoro</Nav.Link>
                 </Col>
               </Row>
             </Link>
-            <Link to="/home" style={{ textDecoration: "none" }}>
+            <Link to="/messaggistica" style={{ textDecoration: "none" }}>
               <Row className="d-flex flex-column ">
                 <Col className="d-flex justify-content-center">
                   <ChatDotsFill />
                 </Col>
                 <Col>
-                  <Nav.Link href="#action1">Messaggistica</Nav.Link>
+                  <Nav.Link href="#action4">Messaggistica</Nav.Link>
                 </Col>
               </Row>
             </Link>
-            <Link to="/home" style={{ textDecoration: "none" }}>
+            <Link to="/notifiche" style={{ textDecoration: "none" }}>
               <Row className="d-flex flex-column ">
                 <Col className="d-flex justify-content-center">
                   <BellFill />
                 </Col>
                 <Col>
-                  <Nav.Link href="#action1">Notifiche</Nav.Link>
+                  <Nav.Link href="#action5">Notifiche</Nav.Link>
                 </Col>
               </Row>
             </Link>
+            <Row className="d-flex flex-column ">
+              <Col className="d-flex justify-content-center">
+                <Image
+                  className="w-25"
+                  roundedCircle
+                  src="https://images.unsplash.com/photo-1507525428034-b723cf961d3e?q=80&w=2073&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                />
+              </Col>
+              <Col className="d-flex justify-content-center">
+                <NavDropdown title="Link" id="navbarScrollingDropdown">
+                  <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
+                  <NavDropdown.Item href="#action4">Another action</NavDropdown.Item>
+                  <NavDropdown.Divider />
+                  <NavDropdown.Item href="#action5">Something else here</NavDropdown.Item>
+                </NavDropdown>
+              </Col>
+            </Row>
 
             <Nav.Link href="#action1">Home</Nav.Link>
             <Nav.Link href="#action2">Link</Nav.Link>
