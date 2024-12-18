5ba13ebb87a8ea266c41d9f571fed67b95a0dc6f
xAlatariel <Orlandielia82@gmail.com>
2024-12-18 12:39:26 +0100

    sidebar-right

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,22 @@ import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Profile from "./components/Profile";
 import MyNavbar from "./components/Navbar";
+import SidebarRight from "./components/SidebarRight";
 
 function App() {
   return (
     <BrowserRouter>
       <MyNavbar />
       <Routes>
-        <Route path="/" element={<Profile />} />
+        <Route
+          path="/"
+          element={
+            <>
+              <Profile />
+              <SidebarRight /> 
+            </>
+          }
+        />
       </Routes>
     </BrowserRouter>
   );
